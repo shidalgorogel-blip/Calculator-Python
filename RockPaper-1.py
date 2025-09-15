ef1301cec0ad78a09e31b9c25dee85cbf89df905
shidalgorogel-blip <shidalgorogel@mail.immaculata.edu>
2025-09-14 21:53:12 -0400

    Add files via upload

---
+++ RockPaper-1.py
@@ -0,0 +1,9 @@
+#Rock paper scissors game.
+
+print('Welcome to this mini game!')
+
+rock= "rock"
+paper= "paper"
+scissors= "scissors"
+user_choice= input("Choose rock, paper, or scissors: ")
+print("You chose: " + user_choice)
